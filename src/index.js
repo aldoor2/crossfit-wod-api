5a78b5f71cbb2b685fcb38126620c5505f7b97dc
Alfonso Puerta <apuertaj2@gmail.com>
2022-09-08 00:02:28 -0500

    Added middleware to server express

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import express from 'express'
 
 import v1WorkoutRouter from './v1/routes/workout.routes.js'
 
+// Initialize
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Middlewares
+app.use(express.json())
+
+// Routes
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT)
