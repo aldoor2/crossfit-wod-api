f79cf78bd398bc366549ca88f39b0c7bb2b08923
Alfonso Puerta <apuertaj2@gmail.com>
2022-09-07 21:20:19 -0500

    Project init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.listen(PORT)
+console.log('🚀 Server is running on port', PORT)
