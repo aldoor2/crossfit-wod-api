48c08fd8185c4aadae816dfb1c2b50c1ed9e7db4
Alfonso Puerta <apuertaj2@gmail.com>
2022-09-07 23:18:46 -0500

    Added service

--- src/controllers/workout.controller.js
+++ src/controllers/workout.controller.js
@@ -1,20 +1,26 @@
+import workoutService from '../services/workout.service.js'
 
 export const getAllWorkouts = (req, res) => {
+  const allWorkouts = workoutService.getAllWorkouts()
   res.send('getting all workouts')
 }
 
 export const getOneWorkout = (req, res) => {
+  const workout = getOneWorkout(req.params.workoutId)
   res.send(`getting workout ${req.params.workoutId}`)
 }
 
 export const createNewWorkout = (req, res) => {
+  const createdWorkout = workoutService.createNewWorkout()
   res.send('creating new workout')
 }
 
 export const deleteOneWorkout = (req, res) => {
+  workoutService.deleteOneWorkout(req.params.workoutId)
   res.send(`deleting workout ${req.params.workoutId}`)
 }
 
 export const updateOneWorkout = (req, res) => {
+  const updatedWorkout = workoutService.updateOneWorkout(req.params.workoutId, req.body)
   res.send(`updating workout ${req.params.workoutId}`)
 }
---
+++ src/services/workout.service.js
@@ -0,0 +1,14 @@
+
+const getAllWorkouts = () => { return }
+const getOneWorkout = (workoutId) => { return }
+const createNewWorkout = () => { return }
+const deleteOneWorkout = (workoutId) => { return }
+const updateOneWorkout = (workoutId, newFields) => { return }
+
+export default {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  deleteOneWorkout,
+  updateOneWorkout
+}
