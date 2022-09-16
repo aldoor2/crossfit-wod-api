5732f15fd85de0ab7507c83941eed68d74f2e92d
Alfonso Puerta <apuertaj2@gmail.com>
2022-09-16 08:51:02 -0500

    Added data caching to workouts and members

--- src/v1/routes/member.routes.js
+++ src/v1/routes/member.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
+import apicache from 'apicache'
 
 import memberController from '../../controllers/member.controller.js'
 
 const router = Router()
+const cache = apicache.middleware
 
 router
-  .get('/', memberController.getAllMembers)
+  .get('/', cache('2 minutes'), memberController.getAllMembers)
   .get('/:memberId', memberController.getOneMember)
   .post('/', memberController.createNewMember)
   .patch('/:memberId', memberController.updateOneMember)
--- src/v1/routes/workout.routes.js
+++ src/v1/routes/workout.routes.js
@@ -1,14 +1,16 @@
 import { Router } from 'express'
+import apicache from 'apicache'
 
 import workoutController from '../../controllers/workout.controller.js'
 import recordController from '../../controllers/record.controller.js'
 
 const router = Router()
+const cache = apicache.middleware
 
 router
-  .get('/', workoutController.getAllWorkouts)
+  .get('/', cache('2 minutes'), workoutController.getAllWorkouts)
   .get('/:workoutId', workoutController.getOneWorkout)
-  .get('/:workoutId/records', recordController.getRecordForWorkout)
+  .get('/:workoutId/records', cache('2 minutes'), recordController.getRecordForWorkout)
   .post('/', workoutController.createNewWorkout)
   .delete('/:workoutId', workoutController.deleteOneWorkout)
   .patch('/:workoutId', workoutController.updateOneWorkout)
